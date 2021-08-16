0e91fd147dc43636786c727b7e2577485c7547d7
mahimayadav24 <88049838+mahimayadav24@users.noreply.github.com>
2021-08-16 16:39:05 +0530

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,22 @@
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar-fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+$(document).ready(function() {
+    if ($('#accordion').length) {
+            $('#accordion').find('.accordion-toggle').click(function() {
+                    if ($(this).hasClass('open')) {
+                            $(this).next().slideUp('fast');
+                            $(this).removeClass('open');
+                    } else {
+                            $(this).next().slideDown('fast');
+                            $(this).addClass('open');
+                            $(".accordion-content").not($(this).next()).slideUp('fast');
+                            $(".accordion-toggle").not($(this)).removeClass('open');
+                    }
+            });
+    }
+});
